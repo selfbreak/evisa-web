3693d346948e80e98aa34df24d601ce9d7598d44
aspyong <aspyong@163.com>
2025-05-06 17:49:24 +0800

    s

--- assets/js/index-DwsA7Q3w.js
+++
@@ -1,1 +0,0 @@
-import{x as t}from"./index-D-IVHDoI.js";function o({path:o,isCurrent:e}){t.confirm({title:"提示",content:"因该框架兼容性问题，而业务相关页面代码需保持和我内部版一致。导致该按钮无法完成页面关闭功能。若需要关闭该页面，请从页面标签处点击'x'关闭",okText:"确认",maskClosable:!0})}export{o as r};
